97011bb6588b261a2139e9ec646c208196b1eaca
aaronandyhealy <aaronhealy2010@hotmail.com>
2018-04-10 17:18:31 +0100

    Basic App Complete

--- WeatherApp/WeatherApp/MainPage.xaml.cs
+++ WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -16,13 +16,10 @@ using Windows.UI.Xaml.Navigation;
 using static WeatherApp.App.WeatherMap;
 
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace WeatherApp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+  
     public sealed partial class MainPage : Page
     {
         public MainPage()
@@ -42,6 +39,8 @@ namespace WeatherApp
             WeatherTextBlock.Text = myWeather.weather[0].description;
 
         }
+
+      
 
     }
 }
